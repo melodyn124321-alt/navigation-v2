911acc44e3fd0daabbe12d78b65a4911507cc03c
seeed upload <seeed-upload@local>
2026-08-12 20:49:09 +0800

    Upload navigation-v2: env/Livox-SDK2

---
+++ env/Livox-SDK2/build/samples/multi_lidars_upgrade/CMakeFiles/multi_lidars_upgrade.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/seeed/ros2/env/Livox-SDK2/samples/multi_lidars_upgrade/main.cpp" "samples/multi_lidars_upgrade/CMakeFiles/multi_lidars_upgrade.dir/main.cpp.o" "gcc" "samples/multi_lidars_upgrade/CMakeFiles/multi_lidars_upgrade.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
